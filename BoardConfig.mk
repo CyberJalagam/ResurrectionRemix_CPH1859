5695ed77a8682975b5c7e415360053e30f3c08dd
buddi56 <ynaveenk.0513@gmail.com>
2020-07-06 15:59:24 +0530

    Remove obsolete dex settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,14 +118,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),user)
-    WITH_DEXPREOPT ?= true
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-  endif
-endif
-
 # Exclude AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
